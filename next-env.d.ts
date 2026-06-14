77e298162104a725e3c3713dbd8cac0c3daceba2
ott0111 <opeyemiadegoke77@gmail.com>
2026-06-13 20:39:08 -0400

    Add TS env and PayPal global types; set Node engine

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+// This file is automatically added by Next.js to provide framework types.
+// Do not edit -- if missing, Next's build may warn about TypeScript setup.
+
+export {}
